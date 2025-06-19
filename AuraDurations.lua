80bc0ad3c6f5acc42d009f9831934a65187c4027
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2025-06-19 11:55:30 +0200

    ref

--- AuraDurations.lua
+++ AuraDurations.lua
@@ -75,7 +75,7 @@ frame:RegisterEvent("PLAYER_LOGIN")
 function frame:PLAYER_LOGIN(event, ...)
     -- print(event, ...)
 
-    if type(AuraDurationsDB) ~= 'table' or true then
+    if type(AuraDurationsDB) ~= 'table' then
         -- print('AuraDurations: new DB!')
         AuraDurationsDB = {}
         lib.AuraDurationsDB = AuraDurationsDB;
@@ -104,6 +104,60 @@ local function ShouldAuraBeLarge(caster)
 
     for token, value in pairs(PLAYER_UNITS) do
         if UnitIsUnit(caster, token) or UnitIsOwnerOrControllerOfUnit(token, caster) then return value; end
+    end
+end
+
+frame.UpdateAuraPositions = function(self, auraName, numAuras, numOppositeAuras, largeAuraList, updateFunc, maxRowWidth,
+                                     offsetX, mirrorAurasVertically)
+    -- custom
+    local AURA_OFFSET_Y = AuraDurationsDB.auraOffsetY;
+    local LARGE_AURA_SIZE = AuraDurationsDB.auraSizeLarge;
+    local SMALL_AURA_SIZE = AuraDurationsDB.auraSizeSmall;
+    local AURA_ROW_WIDTH = AuraDurationsDB.auraRowWidth;
+    local NUM_TOT_AURA_ROWS = AuraDurationsDB.numTotAuraRows;
+    --
+
+    -- Position auras
+    local size;
+    local offsetY = AURA_OFFSET_Y;
+    -- current width of a row, increases as auras are added and resets when a new aura's width exceeds the max row width
+    local rowWidth = 0;
+    local firstBuffOnRow = 1;
+    for i = 1, numAuras do
+        -- update size and offset info based on large aura status
+        if (largeAuraList[i]) then
+            size = LARGE_AURA_SIZE;
+            offsetY = AURA_OFFSET_Y + AURA_OFFSET_Y;
+        else
+            size = SMALL_AURA_SIZE;
+        end
+
+        -- anchor the current aura
+        if (i == 1) then
+            rowWidth = size;
+            self.auraRows = self.auraRows + 1;
+        else
+            rowWidth = rowWidth + size + offsetX;
+        end
+        if (rowWidth > maxRowWidth) then
+            -- this aura would cause the current row to exceed the max row width, so make this aura
+            -- the start of a new row instead
+            updateFunc(self, auraName, i, numOppositeAuras, firstBuffOnRow, size, offsetX, offsetY,
+                       mirrorAurasVertically);
+
+            rowWidth = size;
+            self.auraRows = self.auraRows + 1;
+            firstBuffOnRow = i;
+            offsetY = AURA_OFFSET_Y;
+
+            if (self.auraRows > NUM_TOT_AURA_ROWS) then
+                -- if we exceed the number of tot rows, then reset the max row width
+                -- note: don't have to check if we have tot because AURA_ROW_WIDTH is the default anyway
+                maxRowWidth = AURA_ROW_WIDTH;
+            end
+        else
+            updateFunc(self, auraName, i, numOppositeAuras, i - 1, size, offsetX, offsetY, mirrorAurasVertically);
+        end
     end
 end
 
@@ -208,60 +262,6 @@ frame.TargetBuffHook = function(self)
                               TargetFrame_UpdateDebuffAnchor, maxRowWidth, 3, mirrorAurasVertically);
     -- update the spell bar position
     if (self.spellbar) then Target_Spellbar_AdjustPosition(self.spellbar); end
-end
-
-frame.UpdateAuraPositions = function(self, auraName, numAuras, numOppositeAuras, largeAuraList, updateFunc, maxRowWidth,
-                                     offsetX, mirrorAurasVertically)
-    -- custom
-    local AURA_OFFSET_Y = AuraDurationsDB.auraOffsetY;
-    local LARGE_AURA_SIZE = AuraDurationsDB.auraSizeLarge;
-    local SMALL_AURA_SIZE = AuraDurationsDB.auraSizeSmall;
-    local AURA_ROW_WIDTH = AuraDurationsDB.auraRowWidth;
-    local NUM_TOT_AURA_ROWS = AuraDurationsDB.numTotAuraRows;
-    --
-
-    -- Position auras
-    local size;
-    local offsetY = AURA_OFFSET_Y;
-    -- current width of a row, increases as auras are added and resets when a new aura's width exceeds the max row width
-    local rowWidth = 0;
-    local firstBuffOnRow = 1;
-    for i = 1, numAuras do
-        -- update size and offset info based on large aura status
-        if (largeAuraList[i]) then
-            size = LARGE_AURA_SIZE;
-            offsetY = AURA_OFFSET_Y + AURA_OFFSET_Y;
-        else
-            size = SMALL_AURA_SIZE;
-        end
-
-        -- anchor the current aura
-        if (i == 1) then
-            rowWidth = size;
-            self.auraRows = self.auraRows + 1;
-        else
-            rowWidth = rowWidth + size + offsetX;
-        end
-        if (rowWidth > maxRowWidth) then
-            -- this aura would cause the current row to exceed the max row width, so make this aura
-            -- the start of a new row instead
-            updateFunc(self, auraName, i, numOppositeAuras, firstBuffOnRow, size, offsetX, offsetY,
-                       mirrorAurasVertically);
-
-            rowWidth = size;
-            self.auraRows = self.auraRows + 1;
-            firstBuffOnRow = i;
-            offsetY = AURA_OFFSET_Y;
-
-            if (self.auraRows > NUM_TOT_AURA_ROWS) then
-                -- if we exceed the number of tot rows, then reset the max row width
-                -- note: don't have to check if we have tot because AURA_ROW_WIDTH is the default anyway
-                maxRowWidth = AURA_ROW_WIDTH;
-            end
-        else
-            updateFunc(self, auraName, i, numOppositeAuras, i - 1, size, offsetX, offsetY, mirrorAurasVertically);
-        end
-    end
 end
 
 -- based on blizz: function CompactUnitFrame_UtilSetBuff(buffFrame, unit, index, filter)
